feaaa8e4ec81e2823ea9f1d7f6f108df9abe7655
rosh2001 <91378243+rosh2001@users.noreply.github.com>
2022-04-10 22:18:19 -0500

    Create UploadView.vue

---
+++ src/views/UploadView.vue
@@ -0,0 +1,24 @@
+<template>
+<div class="upload container">
+    <h2>The Form</h2>
+    <UploadForm/>
+ </div>
+
+</template>
+<script>
+import UploadForm from "../components/UploadForm.vue";
+export default {
+    data() {
+    return {};
+    },
+    components: { UploadForm }
+};
+</script>
+<style>
+.hide{
+    visibility: hidden;
+}
+.show{
+    visibility: visible;
+}
+</style>
